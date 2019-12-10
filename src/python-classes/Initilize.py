01a7f89df940f0a0ed08a23530385e9e26c4f271
awmyhr <awmyhr@gmail.com>
2019-12-10 16:36:07 -0500

    initial revamping of Preamble, Initilize, and Postscript done

--- src/python-classes/Initilize.py
+++ src/python-classes/Initilize.py
@@ -16,30 +16,16 @@ from __future__ import with_statement   #: Clean up some uses of try/except PEP-
 from __future__ import print_function   #: Makes print a function, not a statement PEP-3105
 from __future__ import unicode_literals #: Introduce bytes type for older strings PEP-3112
 import logging      #: Python's standard logging facilities
-import ConfigParser
+try:
+    import ConfigParser #: 'Easy' configuration parsing
+except ImportError:
+    import configparser as ConfigParser#: At some point this was required...
 #-- NOTE: We use optparse for compatibility with python < 2.7 as
 #--       argparse wasn't standard until 2.7 (2.7 deprecates optparse)
 #--       As of 20161212 the template is coded for optparse only
 import optparse #: pylint: disable=deprecated-module
 import os
 import sys
-#==============================================================================
-#-- These are only here to support the class
-__version__ = '0.1.0-alpha'
-__revised__ = '20191210-114118'
-__contact__ = 'awmyhr <awmyhr@gmail.com>'
-__synopsis__ = 'Experiments with the CyberArk API'
-__description__ = '''Functionality to be added.
-'''
-__author__ = ['awmyhr <awmyhr@gmail.com>']
-__created__ = '2019-12-10'
-__copyright__ = '2019, awmyhr'
-__license__ = 'TODO: CHANGEME'
-__gnu_version__ = False #: If True print GNU version string (which includes copyright/license)
-__cononical_name__ = 'RunOptions'
-__project_name__ = 'nixTools from *NIXLand'
-__project_home__ = 'https://github.com/awmyhr/nixTools'
-__basename__ = os.path.basename(sys.argv[0]) #: name script run as
 ##--==
 #==============================================================================
 class _ModOptionParser(optparse.OptionParser):
@@ -61,17 +47,18 @@ class _ReSTHelpFormatter(optparse.HelpFormatter):
 
     '''
 
-    def __init__(self, indent_increment=0, max_help_position=4, width=80, short_first=0):
-        optparse.HelpFormatter.__init__(self, indent_increment,
-                                        max_help_position, width, short_first
+    def __init__(self, name=None):
+        optparse.HelpFormatter.__init__(self, indent_increment=0, max_help_position=4,
+                                        width=80, short_first=0
                                        )
+        self.name = name
 
     def format_usage(self, usage):
-        retval = ['%s\n' % ('=-'[self.level] * len(__cononical_name__))]
-        retval.append('%s\n' % (__cononical_name__))
-        retval.append('%s\n\n' % ('=-'[self.level] * len(__cononical_name__)))
+        retval = ['%s\n' % ('=-'[self.level] * len(self.name))]
+        retval.append('%s\n' % (self.name))
+        retval.append('%s\n\n' % ('=-'[self.level] * len(self.name)))
         retval.append('%s' % self.format_heading('Synopsis'))
-        retval.append('**%s** %s\n\n' % (__cononical_name__, usage))
+        retval.append('**%s** %s\n\n' % (self.name, usage))
         return ''.join(retval)
 
     def format_heading(self, heading):
@@ -121,7 +108,7 @@ class _ReSTHelpFormatter(optparse.HelpFormatter):
 
 
 #==============================================================================
-class Initilize(object):
+class Initilize(object): #: pylint: disable=useless-object-inheritance
     ''' Parse the configuration and options; set up logging
 
         Returns:
@@ -131,12 +118,13 @@ class Initilize(object):
     __version = '2.0.0'
 
     _defaults = {
-        'debug': False,
+        'debug': False
     }
 
     _arguments = None
     _configs = None
     _options = None
+    _logger_file_set = False
 
     @property
     def args(self):
@@ -155,20 +143,23 @@ class Initilize(object):
     @property
     def ansible_called(self):
         ''' Class property '''
-        return bool(__basename__.startswith('ansible_module'))
+        return bool(self.mvars['basename'].startswith('ansible_module'))
 
-    def __init__(self, args=None, vars=None):
-        if vars is None:
+    def __init__(self, args=None, mvars=None):
+        if mvars is None:
             raise ValueError('A metavars dict is required!')
-        self.logger = logging.getLogger(vars['name'])
+        self.mvars = mvars
+        self.logger = logging.getLogger(mvars['name'])
         self.logger.setLevel(logging.DEBUG)
         if self._configs is not None:
             raise ValueError('Configs already initialized.')
-        self._configs = self._load_configs(vars['name'])
+        self._configs = self._load_configs(mvars['name'])
         if self._options is not None:
             raise ValueError('Arguments already initialized.')
-        (self._options, self._arguments) = self._parse_args(args, vars['script'])
-        self._init_logger()
+        (self._options, self._arguments) = self._parse_args(args, mvars['script'])
+        self._init_logger(mvars['env'])
+        if self.debug or self._logger_file_set:
+            self._debug_info(mvars['script'])
 
     def _load_configs(self, name):
         parser = ConfigParser.SafeConfigParser(defaults=self._defaults)
@@ -194,7 +185,7 @@ class Initilize(object):
         usage_string = '%s [options]' % (script['basename'])
         version_string = '%s (%s) %s' % (script['cononical_name'],
                                          script['project_name'], script['version'])
-        if __gnu_version__:
+        if self.mvars['flags']['gnu_version']:
             version_string += '\nCopyright %s\nLicense %s\n' % (script['copyright'],
                                                                 script['license'])
         parser = _ModOptionParser(version=version_string, usage=usage_string,
@@ -215,7 +206,7 @@ class Initilize(object):
 
         parsed_opts, parsed_args = parser.parse_args(args)
         if parsed_opts.helprest:
-            parser.formatter = _ReSTHelpFormatter()
+            parser.formatter = _ReSTHelpFormatter(name=script['cononical_name'])
             parser.usage = '[*options*]'         #: pylint: disable=attribute-defined-outside-init
                                                  #: Not yet sure of a better way to do this...
             parser.description = script['description'] #: pylint: disable=attribute-defined-outside-init
@@ -226,23 +217,23 @@ class Initilize(object):
 
         return parsed_opts, parsed_args
 
-    def _init_logger(self):
+    def _init_logger(self, env):
         ''' Initilze logger '''
         if self.debug:
             level = logging.DEBUG
             formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s',
-                                          __logger_dsf__
+                                          env['logger_dsf']
                                          )
         else:
-            if __logger_lvl__.isdigit():
-                if int(__logger_lvl__) > 49:
+            if env['logger_lvl'].isdigit():
+                if int(env['logger_lvl']) > 49:
                     level = logging.CRITICAL
-                elif int(__logger_lvl__) < 10:
+                elif int(env['logger_lvl']) < 10:
                     level = logging.NOTSET
                 else:
-                    level = (int(__logger_lvl__)) //10 * 10
+                    level = (int(env['logger_lvl'])) //10 * 10
             else:
-                level = logging.getLevelName(__logger_lvl__.upper())
+                level = logging.getLevelName(env['logger_lvl'].upper())
             #-- Yes, we are going to ignore unknown values by setting to INFO
             if isinstance(level, str) and level.startswith('Level'):
                 level = logging.INFO
@@ -255,46 +246,48 @@ class Initilize(object):
         self.logger.addHandler(console)
 
         #-- File output
-        if __logger_file__:
-            if os.path.isfile(__logger_file__):
-                os.rename(__logger_file__, '%s.%s' % (__logger_file__, timestamp(__backup_dsf__)))
+        if env['logger_file']:
+            import time
+            if os.path.isfile(env['logger_file']):
+                os.rename(env['logger_file'], '%s.%s' % (env['logger_file'],
+                                                         time.strftime(env['backup_dsf']).strip('+')))
             #: NOTE: In Python >= 2.6 normally I give FileHandler 'delay="true"'
-            logfile = logging.FileHandler(__logger_file__)
+            logfile = logging.FileHandler(env['logger_file'])
             logfile.setLevel(logging.DEBUG)
             formatter = logging.Formatter(
                 '%(asctime)s.%(msecs)d:%(levelno)s:%(name)s.%(funcName)s:%(lineno)d:%(message)s',
-                __logger_dsf__
+                env['logger_dsf']
                 )
             logfile.setFormatter(formatter)
             self.logger.addHandler(logfile)
-            global __logger_file_set__               #: pylint: disable=global-statement
-            __logger_file_set__ = True
+            self._logger_file_set = True
 
+    def _debug_info(self, script):
         import platform #: Easily get platforms identifying info
-        self.logger.debug('Version:   %s (%s) %s', __cononical_name__, __project_name__, __version__)
-        self.logger.debug('Created:   %s / Revised: %s', __created__, __revised__)
+        self.logger.debug('Version:   %s (%s) %s', script['cononical_name'],
+                          script['project_name'], script['version'])
+        self.logger.debug('Created:   %s / Revised: %s', script['created'], script['revised'])
         self.logger.debug('Abs Path:  %s', os.path.abspath(sys.argv[0]))
         self.logger.debug('Full Args: %s', ' '.join(sys.argv[:]))
         self.logger.debug('Python:    %s (%s)', sys.executable, platform.python_version())
-        self.logger.debug('Coder(s):  %s', __author__)
-        self.logger.debug('Contact:   %s', __contact__)
-        self.logger.debug('Project Home: %s', __project_home__)
-        self.logger.debug('Template Version: %s', __template_version__)
+        self.logger.debug('Coder(s):  %s', script['author'])
+        self.logger.debug('Contact:   %s', script['contact'])
+        self.logger.debug('Project Home: %s', ['project_home'])
+        self.logger.debug('Template Version: %s', script['template_version'])
         self.logger.debug('System:    %s', platform.system_alias(platform.system(),
-                                                                platform.release(),
-                                                                platform.version()
-                                                               )
-                        )
+                                                                 platform.release(),
+                                                                 platform.version()
+                                                                )
+                         )
         self.logger.debug('Platform:  %s', platform.platform())
         self.logger.debug('Hostname:  %s', platform.node())
         self.logger.debug('Logname:   %s', os.getlogin())
         self.logger.debug('[re]uid:  %s/%s', os.getuid(), os.geteuid())
         self.logger.debug('PID/PPID:  %s/%s', os.getpid(), os.getppid())
-        if self.options._options is not None:             #: pylint: disable=protected-access
-            self.logger.debug('Parsed Options: %s', self.options._options) #: pylint: disable=protected-access
+        if self._options is not None:             #: pylint: disable=protected-access
+            self.logger.debug('Parsed Options: %s', self._options) #: pylint: disable=protected-access
         if self.debug:
             print('\n----- start -----\n')
-        return
 
 
 ##==---
