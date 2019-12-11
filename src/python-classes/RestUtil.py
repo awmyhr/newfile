9494601bd9d8be3b60e3333be30d4e2fe9a19dfa
awmyhr <awmyhr@gmail.com>
2019-12-11 14:07:42 -0500

    RestUtil should now be able to handle client-id/token based access

--- src/python-classes/RestUtil.py
+++ src/python-classes/RestUtil.py
@@ -6,7 +6,7 @@
 # Proj Home:  https://github.com/awmyhr/newfile
 # Copyright:  2019 awmyhr
 # License:    Apache-2.0
-# Revised:    20191211-101303
+# Revised:    20191211-140742
 # Created:    2019-12-10
 ''' My base class for dealing with REST APIs '''
 #===============================================================================
@@ -28,6 +28,7 @@ from cookielib import LWPCookieJar
 from urllib import urlencode
 import base64
 import json
+import time
 try:
     import requests
 except ImportError:
@@ -38,40 +39,36 @@ class RestUtil(object): #: pylint: disable=useless-object-inheritance
     __version = '2.0.0'
 
     per_page = 100
+    verbose = False
 
-    def __init__(self, authkey=None, insecure=False,
-                 token=None, client_id=None, cookiefile=None):
+    def __init__(self, authkey=None, client_id=None, insecure=False,
+                 cookiefile=None, tokenfile=None, tokenurl=None):
         self.logger = logging.getLogger(__cononical_name__)
         self.logger.debug('Initiallizing RestUtil version %s.', self.__version)
         self.logger.debug(locals())
-        if token is not None:
-            authorization = 'Bearer %s' % token['access_token']
-        else:
-            if authkey is None:
-                raise RuntimeError('authkey not provided.')
-            authorization = 'Basic %s' % authkey
-        self.connection = requests.Session()
-        self.connection.headers = {
-            'x-ibm-client-id': client_id,
-            'content-type': 'application/json',
-            'authorization': authorization,
-            'accept': 'application/json',
-            'cache-control': 'no-cache'
-        }
-        self.logger.debug('Headers set: %s', self.connection.headers)
-        self.connection.verify = not bool(insecure)
-        self.cookiefile = cookiefile
-        if cookiefile is not None:
-            self.connection.cookies = LWPCookieJar(cookiefile)
+        self.token_file = tokenfile
+        self.cookie_file = cookiefile
+
+        self.connection = self._new_connection(authkey=authkey, client_id=client_id, insecure=insecure)
+        if client_id is not None:
+            self.token = self._get_token(tokenfile=self.token_file, tokenurl=tokenurl)
+            self.connection = self._new_connection(authkey=authkey, client_id=client_id, token=self.token, insecure=insecure)
+        if self.cookie_file is not None:
+            self.connection.cookies = LWPCookieJar(self.cookie_file)
             try:
                 self.connection.cookies.load(ignore_discard=True)
             except IOError:
                 pass
-        self.verbose = False
         self.results = {"success": None, "msg": None, "return": None}
 
     def __del__(self):
-        if self.cookiefile is not None:
+        self.logger.debug('Entering Function: %s', sys._getframe().f_code.co_name) #: pylint: disable=protected-access
+        if self.token_file is not None:
+            self.logger.debug('Saving token file: %s.', self.token_file)
+            with open(self.token_file, 'w') as file_:
+                json.dump(self.token, file_)
+        if self.cookie_file is not None:
+            self.logger.debug('Saving cookie file: %s.', self.cookie_file)
             try:
                 self.connection.cookies.save(ignore_discard=True)
             except IOError:
@@ -109,6 +106,63 @@ class RestUtil(object): #: pylint: disable=useless-object-inheritance
     #     except IOError:
     #         pass
     #     return self.cookies
+
+    def _get_token(self, tokenfile=None, tokenurl=None):
+        ''' Retrive a Bearer token
+
+        Returns:
+            Token as a dict
+
+        '''
+        self.logger.debug('Entering Function: %s', sys._getframe().f_code.co_name) #: pylint: disable=protected-access
+        try:
+            self.logger.debug('Attempting to load token file: %s.', tokenfile)
+            with open(tokenfile, 'r') as file_:
+                token = json.load(file_)
+            if 'expires' not in token or int(time.time()) > int(token['expires']):
+                self.logger.debug('Token file invalid or expired, getting new token.')
+                self.logger.debug('Time: %s ; Token Exp: %s', int(time.time()), int(token['expires']))
+                self.logger.debug('Token: %s', token)
+                token = self.rest_call('get', tokenurl)
+        except (IOError, AttributeError, TypeError, ValueError):
+            self.logger.debug('Error with token file, requesting new token.')
+            token = self.rest_call('get', tokenurl)
+        if 'access_token' not in token:
+            raise RuntimeError('Error: No access token. Only found: %s' % token)
+        return token
+
+    def _new_connection(self, authkey=None, client_id=None, token=None, insecure=False):
+        ''' Create a Request session object
+
+        Args:
+            authkey (str):   User authorization key
+            insecure (bool): If True do not validate SSL
+            token (dict):    User token as a dict, need 'access_token'
+            client_id (str): User client ID for controlled API access
+
+        Returns:
+            Requests session object.
+
+        '''
+        self.logger.debug('Entering Function: %s', sys._getframe().f_code.co_name) #: pylint: disable=protected-access
+
+        connection = requests.Session()
+        connection.headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'cache-control': 'no-cache'
+        }
+        if token is None:
+            if authkey is not None:
+                connection.headers['authorization'] = 'Basic %s' % authkey
+        else:
+            connection.headers['authorization'] = 'Bearer %s' % token['access_token']
+        if client_id is not None:
+            connection.headers['x-ibm-client-id'] = client_id
+        self.logger.debug('Headers set: %s', connection.headers)
+        connection.verify = not bool(insecure)
+
+        return connection
 
     def rest_call(self, method, url, params=None, data=None, jsonin=None):
         ''' Call a REST API URL using method.
