40fc56990530df12e34fc7acd6ab826987f94af1
awmyhr <awmyhr@gmail.com>
2019-12-13 11:42:33 -0500

    python object to hold user login and related data

---
+++ src/python-classes/UserObject.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python2 -tt
+# -*- coding: utf-8 -*-
+# Author:     awmyhr <awmyhr@gmail.com>
+# Contact:    awmyhr <awmyhr@gmail.com>
+# Project:    newfile
+# Proj Home:  https://github.com/awmyhr/newfile
+# Copyright:  2019 awmyhr
+# License:    Apache-2.0
+# Revised:    20191213-114233
+# Created:    2019-12-13
+''' Hold user login and related data '''
+#==============================================================================
+from __future__ import absolute_import  #: Require parens to group imports PEP-0328
+from __future__ import division         #: Enable 3.x True Division PEP-0238
+from __future__ import with_statement   #: Clean up some uses of try/except PEP--343
+from __future__ import print_function   #: Makes print a function, not a statement PEP-3105
+from __future__ import unicode_literals #: Introduce bytes type for older strings PEP-3112
+#------------------------------------------------------------------------------
+__cononical_name__ = 'UserObject'
+#------------------------------------------------------------------------------
+##--==
+#===============================================================================
+#-- UserObject v0.1.0
+#==============================================================================
+class UserObject(object): #: pylint: disable=useless-object-inheritance
+    ''' Hold user login and related data '''
+    __version = '0.1.0'
+
+    _authkey = None
+    _clientid = None
+    _cookiefile = None
+    _password = None
+    _tokenfile = None
+    _username = None
+
+    def __init__(self, username=None, password=None, authkey=None, clientid=None,
+                 cookiefile=None, tokenfile=None):
+        import logging
+        self.logger = logging.getLogger(__cononical_name__)
+        self.logger.debug('Initiallizing UserObject version %s.', self.__version)
+        if authkey is None:
+            if username is None:
+                username = raw_input('Username: ')
+            if password is None:
+                import getpass
+                password = getpass.getpass('Password: ')
+        self.username = username
+        self.password = password
+        self.clientid = clientid
+        self.tokenfile = tokenfile
+        self.cookiefile = cookiefile
+        if authkey is not None:
+            self.authkey = authkey
+
+    def __str__(self):
+        if self.username is not None:
+            return 'Data for user %s' % self.username
+        return 'Data for authkey %s' % self.authkey
+
+    def __repr__(self):
+        params = []
+        if self._username is not None:
+            params.append('username="%s"' % self.username)
+        if self._password is not None:
+            params.append('password="%s"' % self.password)
+        if self._authkey is not None:
+            params.append('authkey="%s"' % self.authkey)
+        if self._clientid is not None:
+            params.append('clientid="%s"' % self.clientid)
+        if self._cookiefile is not None:
+            params.append('cookiefile="%s"' % self.cookiefile)
+        if self._tokenfile is not None:
+            params.append('tokenfile="%s"' % self.tokenfile)
+        return 'User(%s)' % ', '.join(params)
+
+    @property
+    def clientid(self):
+        ''' instance property '''
+        return self._clientid
+
+    @clientid.setter
+    def clientid(self, value):
+        ''' property setter '''
+        self._clientid = value
+
+    @property
+    def cookiefile(self):
+        ''' instance property '''
+        if self._cookiefile is None:
+            import os
+            return '%s/.%s-%s' % (os.getenv("HOME"), os.getenv('USER'), 'cookie')
+        return self._cookiefile
+
+    @cookiefile.setter
+    def cookiefile(self, value):
+        ''' property setter '''
+        self._cookiefile = value
+
+    @property
+    def tokenfile(self):
+        ''' instance property '''
+        if self._tokenfile is None:
+            import os
+            import tempfile
+            return '%s/.%s-%s' % (tempfile.gettempdir(), os.getenv('USER'), 'token')
+        return self._tokenfile
+
+    @tokenfile.setter
+    def tokenfile(self, value):
+        ''' property setter '''
+        self._tokenfile = value
+
+    @property
+    def username(self):
+        ''' instance property '''
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        ''' property setter '''
+        self._username = value
+        self._gen_authkey()
+
+    @property
+    def password(self):
+        ''' instance property '''
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        ''' property setter '''
+        self._password = value
+        self._gen_authkey()
+
+    @property
+    def authkey(self):
+        ''' instance property '''
+        return self._authkey
+
+    @authkey.setter
+    def authkey(self, value):
+        ''' property setter '''
+        if self._authkey is None:
+            self._authkey = value
+        else:
+            self._gen_authkey()
+            if self._authkey != value:
+                raise ValueError('Username/Password authkey do not match passed authkey (%s / %s).'
+                                 % (self._authkey, value))
+
+    def _gen_authkey(self):
+        ''' instance function to generate base64 authkey '''
+        if self.username is None or self.password is None:
+            self._authkey = None
+        else:
+            import base64
+            self._authkey = base64.b64encode('%s:%s' % (self.username, self.password)).strip()
+
+##==---
+#==============================================================================
+if __name__ == '__main__':
+    from pprint import pprint
+    print('testing 1, 2, 3...')
+    print('=============================')
+
+    test1 = UserObject('test1', '1test')
+    print('test 1a:    %s / %s / %s / %s' % (test1.username, test1.password,
+                                             test1.authkey, test1.clientid))
+    test1.username = 'TEST1'
+    test1.password = '1TEST'
+    print('test 1b:    %s / %s / %s / %s' % (test1.username, test1.password,
+                                             test1.authkey, test1.clientid))
+    print('cookiefile: %s' % (test1.cookiefile))
+    print('tokenfile:  %s' % (test1.tokenfile))
+    test1.cookiefile = '/path/to/cookie/file'
+    test1.tokenfile = '/path/to/token/file'
+    print('cookiefile: %s' % (test1.cookiefile))
+    print('tokenfile:  %s' % (test1.tokenfile))
+    pprint(vars(test1))
+    print(test1)
+    print(repr(test1))
+
+    print('=============================')
+
+    test2 = UserObject('test2', '2test', 'dGVzdDI6MnRlc3Q=')
+    print('test 2a:    %s / %s / %s / %s' % (test2.username, test2.password,
+                                             test2.authkey, test2.clientid))
+    test2.username = 'TEST2'
+    test2.password = '2TEST'
+    print('test 2b:    %s / %s / %s / %s' % (test2.username, test2.password,
+                                             test2.authkey, test2.clientid))
+    pprint(vars(test2))
+    print(test2)
+    print(repr(test2))
+
+    print('=============================')
