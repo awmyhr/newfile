0714de0e989547d4c225363df70d598c987cd06f
awmyhr <awmyhr@gmail.com>
2019-12-16 14:15:10 -0500

    trying to betterify the class

--- src/python-classes/ServerObject.py
+++ src/python-classes/ServerObject.py
@@ -6,7 +6,7 @@
 # Proj Home:  https://github.com/awmyhr/newfile
 # Copyright:  2019 awmyhr
 # License:    Apache-2.0
-# Revised:    20191213-150515
+# Revised:    20191216-141510
 # Created:    2019-12-13
 ''' Hold server access data '''
 #==============================================================================
@@ -27,47 +27,63 @@ class ServerObject(object): #: pylint: disable=useless-object-inheritance
 
         Args:
             name          (str): Required. Server name as a string.
-            api_path      (str): Required. Path of API relative to base url.
             base_url      (str): Over-ride default base url. Default: http://name
-            cookies_used (bool): Does the API use cookies?   Default: False
             insecure     (bool): Ignore SSL verification?    Default: False
-            api_url       (str): Over-ride full URL for API. Default: base_url/api_path
-            token_needed (bool): Token-based login required? Default: False
-            token_url     (str): Full URL for Token authentication.
+            api           (str): Relative path or full URL.
+            api          (dict): API specific information.
+                api['path']: (str): Path of API relative to base url.
+                api['url']:  (str): Over-ride full URL for API.
+            cookies       (str): Path to cookie file. Default: None
+            token         (str): Relative path or full URL. Default: None
+            token        (dict): Information about token. Default: None
+                token['file'] (str): Path to token file. Default: $TMP/.$USER-__cononical_name__
+                token['path'] (str): Path of Token auth relative to base url.
+                token['url']  (str): Full URL for Token authentication.
+
+        A value for api is required. If it's a string, an attempt will be made
+            to identify if its a valid URL or not. If not, it is assumed to be
+            a relative path. If no value is passed, a dict without a path
+            or url key is passed, or an unuseable value is passed, a ValueError
+            will be raised.
+        If token is None (the default), then token authentication is not used.
+            Otherwise, if a string is passed then it will be processed like api
+            above and the default value will be used for the file key. Path is
+            relative to the API url. If a dict is passed, either a path or a
+            url key is required. If an unuseable value is passed, a ValueError
+            will be raised.
+        If cookies is None, then cookies will not be used. Otherwise, should be
+            valid filesystem path to a writeable file.
 
         Returns:
             A ServerObject.'''
-    __version = '0.1.0'
+    __version = '0.2.0'
 
-    _api_path = None
-    _api_url = None
-    _base_url = None
-    _cookies_used = False
-    _insecure = False
-    _name = None
-    _token_needed = False
-    _token_url = None
+    _defaults = {
+        'api': {'path': None, 'url': None},
+        'token': {'file': None, 'path': None, 'url': None}
+    }
 
-    def __init__(self, name=None, api_path=None, base_url=None, cookies_used=False,
-                 insecure=False, api_url=None, token_needed=False, token_url=None):
+    def __init__(self, name=None, base_url=None, insecure=False, api=None,
+                 cookies=None, token=None):
         import logging
-        self.logger = logging.getLogger(__cononical_name__)
-        self.logger.debug('Initiallizing ServerObject version %s.', self.__version)
+        self._logger = logging.getLogger(__cononical_name__)
+        self._logger.debug('Initiallizing ServerObject version %s.', self.__version)
         if name is None:
             raise ValueError('name is a required paramater.')
-        if api_path is None and api_url is None:
-            raise ValueError('either api_path or api_url is required.')
-        self.name = name
-        self.insecure = insecure
-        self.base_url = base_url
-        self.cookies_used = cookies_used
-        self.api_path = api_path
-        self.api_url = api_url
-        self.token_needed = token_needed
-        self.token_url = token_url
+        if api is None:
+            raise ValueError('api is a required paramater.')
+        self._name = name
+        self._insecure = insecure
+        self._base_url = base_url
+        self._cookies = cookies
+        self._api = self._defaults['api']
+        self._token = self._defaults['token']
+
+        self.api = api
+        self.token = token
 
     def __str__(self):
-        return 'Data for authkey %s' % self.name
+        return 'Data for server %s' % self.name
 
     def __repr__(self):
         params = []
@@ -77,39 +93,23 @@ class ServerObject(object): #: pylint: disable=useless-object-inheritance
             params.append('insecure="%s"' % self.insecure)
         if self._base_url is not None:
             params.append('base_url="%s"' % self.base_url)
-        if self._cookies_used is not False:
-            params.append('cookies_used="%s"' % self.cookies_used)
-        if self._api_path is not None:
-            params.append('api_path="%s"' % self.api_path)
-        if self._api_url is not None:
-            params.append('api_url="%s"' % self.api_url)
-        if self._token_needed is not False:
-            params.append('token_needed="%s"' % self.token_needed)
-        if self._token_url is not None:
-            params.append('token_url="%s"' % self.token_url)
+        if self._cookies is not None:
+            params.append('cookies="%s"' % self.cookies)
+        if self._api is not None:
+            params.append('api="%s"' % self.api)
+        if self._token is not None:
+            params.append('token="%s"' % self.token)
         return 'ServerObject(%s)' % ', '.join(params)
 
     @property
-    def api_path(self):
+    def name(self):
         ''' instance property '''
-        return self._api_path
+        return self._name
 
-    @api_path.setter
-    def api_path(self, value):
+    @name.setter
+    def name(self, value):
         ''' property setter '''
-        self._api_path = value
-
-    @property
-    def api_url(self):
-        ''' instance property '''
-        if self._api_url is None:
-            return '%s/%s' % (self.base_url, self.api_path)
-        return self._api_url
-
-    @api_url.setter
-    def api_url(self, value):
-        ''' property setter '''
-        self._api_url = value
+        self._name = value
 
     @property
     def base_url(self):
@@ -124,18 +124,6 @@ class ServerObject(object): #: pylint: disable=useless-object-inheritance
         self._base_url = value
 
     @property
-    def cookies_used(self):
-        ''' instance property '''
-        return self._cookies_used
-
-    @cookies_used.setter
-    def cookies_used(self, value):
-        ''' property setter '''
-        if not isinstance(value, bool):
-            raise ValueError('cookies_used requires bool')
-        self._cookies_used = value
-
-    @property
     def insecure(self):
         ''' instance property '''
         return self._insecure
@@ -148,36 +136,58 @@ class ServerObject(object): #: pylint: disable=useless-object-inheritance
         self._insecure = value
 
     @property
-    def name(self):
+    def cookies(self):
         ''' instance property '''
-        return self._name
+        return self._cookies
 
-    @name.setter
-    def name(self, value):
+    @cookies.setter
+    def cookies(self, value):
         ''' property setter '''
-        self._name = value
+        self._cookies = value
 
     @property
-    def token_needed(self):
+    def api(self):
         ''' instance property '''
-        return self._token_needed
+        return self._api
 
-    @token_needed.setter
-    def token_needed(self, value):
+    @api.setter
+    def api(self, value):
         ''' property setter '''
-        if not isinstance(value, bool):
-            raise ValueError('token_needed requires bool')
-        self._token_needed = value
+        if isinstance(value, dict):
+            if 'path' in value:
+                self._api['path'] = value['path']
+                self._api['url'] = '%s/%s' % (self.base_url, self._api['path'])
+            if 'url' in value:
+                self._api['url'] = value['url']
+            if 'url' not in value and 'path' not in value:
+                raise ValueError('api dict does not include url or path (%s)' % value)
+        elif isinstance(value, basestring):
+            if self._is_url(value):
+                self._api['url'] = value
+            else:
+                self._api['path'] = value
+                self._api['url'] = '%s/%s' % (self.base_url, self._api['path'])
+        else:
+            raise ValueError('unuseable type for api (%s)' % type(value))
 
     @property
-    def token_url(self):
+    def token(self):
         ''' instance property '''
-        return self._token_url
+        return self._token
 
-    @token_url.setter
-    def token_url(self, value):
+    @token.setter
+    def token(self, value):
         ''' property setter '''
-        self._token_url = value
+        self._token = value
+
+    @classmethod
+    def _is_url(cls, value):
+        ''' Check if a string looks like a web URL '''
+        from urlparse import urlparse
+        check = urlparse(value)
+        if check.scheme in ['http', 'https']:
+            return True
+        return False
 
 
 ##==---
@@ -187,19 +197,23 @@ if __name__ == '__main__':
     print('testing 1, 2, 3...')
     print('=============================')
 
-    test1 = ServerObject(name='test1', api_path='api/v1')
-    print(test1.api_url)
+    test1 = ServerObject(name='test1', api='api/v1')
+    print(test1.api)
     print('-----------------------------')
-    print('type: %s ; is ServerObject? %s' % (type(test1), isinstance(test1, ServerObject)))
     pprint(vars(test1))
     print(test1)
     print(repr(test1))
 
     print('=============================')
 
-    if isinstance(test1, ServerObject):
-        print('We got a ServerObject')
-    else:
-        print('ServerObject requried! We got a %s (%s)' % (type(test1), test1.__class__.__name__))
-    print(test1.__doc__)
-    print('=============================')
+    test2 = ServerObject(name='test2', api='http://otherserver/v1/api', cookies='/path/to/cooks')
+    print(test2.api)
+    print('-----------------------------')
+    pprint(vars(test2))
+    print(test2)
+    print(repr(test2))
+
+    # print('=============================')
+    # print('type: %s ; is ServerObject? %s' % (type(test1), isinstance(test1, ServerObject)))
+    # print(test1.__doc__)
+    # print('=============================')
