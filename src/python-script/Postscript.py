5ccecb52853b72ccc1a8668d7c0dd8064eef2f18
awmyhr <awmyhr@gmail.com>
2019-12-18 11:33:51 -0500

    mimic Setup()s new use of class vars instead of instance vars

--- src/python-script/Postscript.py
+++ src/python-script/Postscript.py
@@ -7,7 +7,7 @@
 # Proj Home:  https://github.com/awmyhr/newfile
 # Copyright:  2019 awmyhr
 # License:    Apache-2.0
-# Revised:    20191217-145437
+# Revised:    20191218-113351
 # Created:    2019-12-10
 from __future__ import absolute_import  #: Require parens to group imports PEP-0328
 from __future__ import division         #: Enable 3.x True Division PEP-0238
@@ -25,27 +25,37 @@ METAVARS = {
     'basename': 'postscript.py',
     'flags': {'require_root': False}
 }
-class Initilize(object): #: pylint: disable=useless-object-inheritance,too-few-public-methods
+class Setup(object): #: pylint: disable=useless-object-inheritance,too-few-public-methods
     ''' dummy class for testing '''
-    def __init__(self, args, mvars):
-        self.logger = logging.getLogger(__cononical_name__)
-        self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
-        console = logging.StreamHandler()
-        console.setLevel(logging.DEBUG)
-        console.setFormatter(formatter)
-        self.logger.addHandler(console)
-        self.args = args
-        self.mvars = mvars
-        self.debug = False
+    _logger = logging.getLogger(__cononical_name__)
+    args = None
+    mvars = None
+    debug = False
+    _is_init_done = False
+
+    def __init__(self, args=None, mvars=None):
+        if Setup._is_init_done:
+            pass
+        else:
+            Setup._logger.setLevel(logging.DEBUG)
+            formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+            console = logging.StreamHandler()
+            console.setLevel(logging.DEBUG)
+            console.setFormatter(formatter)
+            Setup._logger.addHandler(console)
+            if Setup.args is None:
+                Setup.args = args
+            if Setup.mvars is None:
+                Setup.mvars = mvars
+            Setup._is_init_done = True
 
 ##--==
 #==============================================================================
 #-- Postscript v2.1.0
 #==============================================================================
-def main(options):
-    ''' This is where the action takes place
-    '''
+def main():
+    ''' This is where the magic takes place '''
+    options = Setup()
     logger = logging.getLogger(options.mvars['name'])
     logger.debug('Starting main()')
     #-- TODO: Do something more interesting here...{#
@@ -63,7 +73,7 @@ def main(options):
 #==============================================================================
 if __name__ == '__main__':
     #-- Parse configs/envs/options and set up logging
-    OPTS = Initilize(args=sys.argv[1:], mvars=METAVARS)
+    OPTS = Setup(args=sys.argv[1:], mvars=METAVARS)
     LOGGER = logging.getLogger(OPTS.mvars['name'])
     if OPTS.mvars['flags']['require_root'] and os.getegid() != 0:
         LOGGER.error('Must be run as root.')
@@ -77,7 +87,7 @@ if __name__ == '__main__':
     #         will no longer be able to warn on object members...
     #         type, value, traceback = sys.exc_info()
     try:
-        main(OPTS)
+        main()
     except SystemExit as error: # Catches sys.exit()
         #_, error, _ = sys.exc_info()
         LOGGER.debug('Caught SystemExit')
